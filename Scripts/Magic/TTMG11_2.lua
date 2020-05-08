9085275ce9cf4648a0b9652b2b758b675e7f4e6a
xArshy <emamdeearshad01@gmail.com>
2020-05-08 20:23:48 +0400

    Translate

--- Scripts/Magic/TTMG11_2.lua
+++ Scripts/Magic/TTMG11_2.lua
@@ -1,44 +1,43 @@
-﻿local tbTable = GameMain:GetMod("MagicHelper");
-local tbMagic = tbTable:GetMagic("TTMG_11_2");
+﻿local tbTable = GameMain:GetMod("MagicHelper")
+local tbMagic = tbTable:GetMagic("TTMG_11_2")
 
 function tbMagic:EnableCheck(npc)
-	return true;
+    return true
 end
 
 function tbMagic:TargetCheck(key, t)
-	local id = t.ID;
-	if t.IsFaBao == true then
-	return true;
-	else
-	return false;
-	end
+    local id = t.ID
+    if t.IsFaBao then
+        return true
+    else
+        return false
+    end
 end
 
 function tbMagic:MagicEnter(IDs, IsThing)
-	self.targetId = IDs[0];
+    self.targetId = IDs[0]
 end
 
-function tbMagic:MagicStep(dt, duration)	
-	self:SetProgress(duration/self.magic.Param1);
-	if duration >=self.magic.Param1  then
-		return 1;	
-	end
-	return 0;
+function tbMagic:MagicStep(dt, duration)
+    self:SetProgress(duration / self.magic.Param1)
+    if duration >= self.magic.Param1 then
+        return 1
+    end
+    return 0
 end
 
 function tbMagic:MagicLeave(success)
-	if success == true then		
-		local target = ThingMgr:FindThingByID(self.targetId);
-		local AT = target.Fabao:GetProperty(CS.XiaWorld.Fight.g_emFaBaoP.AttackPower)
-		local v1 = self.bind:GetProperty("NpcFight_FabaoPowerAddP");
-	if target ~= nil then
-		if v1 <= 100000000 then
-		self.bind.PropertyMgr:ModifierProperty("NpcFight_FabaoPowerAddP",AT*0.5);
-		target.Fabao:SetProperty(CS.XiaWorld.Fight.g_emFaBaoP.AttackPower,-AT)
-		
-		else
-		self.bind:AddMsg("[NAME]已经提升到了极限，吸不动了。")
-		end
-		end
-	end
+    if success then
+        local target = ThingMgr:FindThingByID(self.targetId)
+        local AT = target.Fabao:GetProperty(CS.XiaWorld.Fight.g_emFaBaoP.AttackPower)
+        local v1 = self.bind:GetProperty("NpcFight_FabaoPowerAddP")
+        if target then
+            if v1 <= 100000000 then
+                self.bind.PropertyMgr:ModifierProperty("NpcFight_FabaoPowerAddP", AT * 0.5)
+                target.Fabao:SetProperty(CS.XiaWorld.Fight.g_emFaBaoP.AttackPower, -AT)
+            else
+                self.bind:AddMsg("[NAME] has reached the limit, cannot absorb.")
+            end
+        end
+    end
 end
--- Scripts/Magic/TTMG2_2.lua
+++ Scripts/Magic/TTMG2_2.lua
@@ -1,57 +1,36 @@
-﻿local tbTable = GameMain:GetMod("MagicHelper");--获取神通模块 这里不要动
-local tbMagic = tbTable:GetMagic("TTMG_2_2");--创建一个新的神通class
-
---注意-
---神通脚本运行的时候有两个固定变量
---self.bind 执行神通的npcObj
---self.magic 当前神通的数据，也就是定义在xml里的数据
+﻿local tbTable = GameMain:GetMod("MagicHelper")
+local tbMagic = tbTable:GetMagic("TTMG_2_2")
 
 function tbMagic:Init()
 end
 
---神通是否可用
 function tbMagic:EnableCheck(npc)
-	return true;
+    return true
 end
 
-
---目标合法检测 首先会通过magic的SelectTarget过滤，然后再通过这里过滤
---IDs是一个List<int> 如果目标是非对象，里面的值就是地点key，如果目标是物体，值就是对象ID，否则为nil
---IsThing 目标类型是否为物体
-function tbMagic:TargetCheck(key, t)	
-	if t.ThingType == CS.XiaWorld.g_emThingType.Npc then
-		return true;
-	else
-		return false;
-	end
+function tbMagic:TargetCheck(key, t)
+    if t.ThingType == CS.XiaWorld.g_emThingType.Npc then
+        return true
+    else
+        return false
+    end
 end
 
-
---开始施展神通
 function tbMagic:MagicEnter(IDs, IsThing)
-	self.targetId = IDs[0];--获取目标信息
-		
-		
+    self.targetId = IDs[0]
 end
 
---施展完成/失败 success是否成功
 function tbMagic:MagicLeave(success)
-	if success == true then		
-		if self.bind ~= nil then
-		
-		self.bind.LuaHelper:TriggerStory("TTMG_2_2");
-		end
-	end
+    if success then
+        if self.bind then
+            self.bind.LuaHelper:TriggerStory("TTMG_2_2")
+        end
+    end
 end
 
-
-
---存档 如果没有返回空 有就返回Table(KV)
 function tbMagic:OnGetSaveData()
-	
 end
 
---读档 tbData是存档数据 IDs和IsThing同进入
-function tbMagic:OnLoadData(tbData,IDs, IsThing)	
-	self.targetId = IDs[0];--获取目标信息
+function tbMagic:OnLoadData(tbData, IDs, IsThing)
+    self.targetId = IDs[0]
 end
