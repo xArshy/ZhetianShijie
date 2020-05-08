62c84dd48780b8c991de37392d732af8f6a63ecd
xArshy <emamdeearshad01@gmail.com>
2020-05-08 20:48:25 +0400

    Translate

--- Scripts/Modifier/dy/HG.lua
+++ Scripts/Modifier/dy/HG.lua
@@ -5,32 +5,27 @@ local tbModifier = tbTable:GetModifier("modifier_HG")
 --注意：自定义modidifer要注意离开的时候将自定义效果移除
 --进入modifier
 function tbModifier:Enter(modifier, npc)
-	npc.LuaHelper:AddPracticeResource("Stage", 99999);
-  npc.LuaHelper:NeckBroken();
+    npc.LuaHelper:AddPracticeResource("Stage", 99999)
+    npc.LuaHelper:NeckBroken()
 end
 
 --modifier step
 function tbModifier:Step(modifier, npc, dt)
-
 end
 
 --层数更新
 function tbModifier:UpdateStack(modifier, npc, add)
-	
 end
 
 --离开modifier
 function tbModifier:Leave(modifier, npc)
-	
 end
 
 --获取存档数据
 function tbModifier:OnGetSaveData()
-	return nil
+    return nil
 end
 
 --载入存档数据
 function tbModifier:OnLoadData(modifier, npc, tbData)
-
 end
-
--- Scripts/Modifier/dy/SLS.lua
+++ Scripts/Modifier/dy/SLS.lua
@@ -5,31 +5,25 @@ local tbModifier = tbTable:GetModifier("modifier_SLS")
 --注意：自定义modidifer要注意离开的时候将自定义效果移除
 --进入modifier
 function tbModifier:Enter(modifier, npc)
-	npc.LuaHelper:AddEnemy(21,0)
+    npc.LuaHelper:AddEnemy(21, 0)
 end
 
 --modifier step
 function tbModifier:Step(modifier, npc, dt)
-
 end
 --层数更新
 function tbModifier:UpdateStack(modifier, npc, add)
-	
 end
 
 --离开modifier
 function tbModifier:Leave(modifier, npc)
-	
-
 end
 
 --获取存档数据
 function tbModifier:OnGetSaveData()
-	return nil
+    return nil
 end
 
 --载入存档数据
 function tbModifier:OnLoadData(modifier, npc, tbData)
-
 end
-
